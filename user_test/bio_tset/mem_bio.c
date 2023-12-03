34f50b4627ee4190d7718024163112507efcfca4
HYJ <294481073@qq.com>
2023-12-03 11:51:14 +0800

    测试文件bio

--- user_test/bio_tset/mem_bio.c
+++ user_test/bio_tset/mem_bio.c
@@ -7,7 +7,11 @@ int main()
     int len = 0;
     char* out = NULL;
 
-    b = BIO_new(BIO_s_mem());
+    b = BIO_new(BIO_s_mem());//参数是一个函数
+    /* char data[] = "Hello World";
+     BIO *mem;
+     mem = BIO_new_mem_buf(data, -1);//可以设置BIO为只读的
+     */
     len = BIO_puts(b, "hello world\n");
     len = BIO_write(b, "openssl", 7);
     len = BIO_printf(b,"%s","zcp");
